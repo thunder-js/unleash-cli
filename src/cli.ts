bf5779ec618f9ed3718dd07fc1dd7d078d0f76a2
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-23 17:38:12 -0200

    Correctly handling errors

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,15 @@
 import * as yargs from 'yargs'
 import reactCLI from './cmd/react/cli'
 import projectCLI from './cmd/project/cli'
+import chalk from 'chalk'
+import { DetailedError } from './common/error'
 
 yargs
-  .command('react', 'create react things', reactCLI)
   .command('project', 'create project things', projectCLI)
+  .command('react', 'create react things', reactCLI)
+  .fail((_, err: DetailedError) => {
+    console.log(chalk.red(err.message))
+    console.log(chalk.grey(err.details))
+  })
   .help()
   .parse()
